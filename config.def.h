849979b38624a5330518c2084e83b119990f1844
Alexvanaxe <alexvanaxe@gmail.com>
2021-02-01 10:58:39 -0300

    Adding a font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Symbols Nerd Font:size=20",
+	"Symbols Nerd Font:size=10",
 	"monospace:size=10"
 
 };
